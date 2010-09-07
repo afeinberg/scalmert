e79f55f46a47036ff9c83276464a2e20d388fbab
Alex Feinberg <alex@strlen.net>
2010-09-06 17:44:30 -0700

    Stylistic changes

--- src/test/scala/voldemort/scalmert/client/StoreClientFactorySpec.scala
+++ src/test/scala/voldemort/scalmert/client/StoreClientFactorySpec.scala
@@ -4,11 +4,10 @@ import scala.collection.mutable.Map
 import org.specs.Specification
 import voldemort.serialization.StringSerializer
 import voldemort.client.MockStoreClientFactory
-
+import voldemort.versioning.{VectorClock, Versioned => JVersioned}
 
 import voldemort.scalmert.Implicits._
 import voldemort.scalmert.versioning._
-import voldemort.versioning.{VectorClock, Versioned => JVersioned}
 
 /**
   *  @author alex
@@ -26,7 +25,6 @@ class  StoreClientFactorySpec extends Specification {
     }
   }
 
- 
   "factory" should {
     "return a valid StoreClient" in {
       client + ("hello" -> Versioned("world"))
@@ -37,21 +35,22 @@ class  StoreClientFactorySpec extends Specification {
   "setting a custom inconsistency resolver" should {
     "be supported" in {
       val resolver: Seq[JVersioned[String]] => Seq[JVersioned[String]] = { results =>
-        val versions = results.map { _.getVersion } 
+        val versions = results.map(_.getVersion)
         val mergedVersion = versions.foldLeft(new VectorClock) { 
           (clock, ver) =>
             clock.merge(ver.asInstanceOf[VectorClock]);
         }
-        val value = results.map( _.getValue ).foldLeft(new StringBuffer) {
+        val value = (results.map(_.getValue).foldLeft(new StringBuffer) {
           (sb, str) =>
             sb.append(str)
-        }.toString
+        }).toString
+
         Seq[JVersioned[String]](Versioned(value, mergedVersion))
       }
 
       val c:StoreClient[String, String] = factory.getStoreClient[String, String]("test", resolver)
-      c + ( "quux" -> Versioned("foo", getClock(List[Int](0))))
-      c + ( "quux" -> Versioned("bar", getClock(List[Int](1))))
+      c + ("quux" -> Versioned("foo", getClock(List(0))))
+      c + ("quux" -> Versioned("bar", getClock(List(1))))
       c("quux").getValue mustEqual "foobar"
     }
   }
